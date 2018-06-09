c2c42756c9d5878836122402507bdfa30f7c37e5
Niach <mrniach@web.de>
2018-06-09 13:28:37 +0200

    Projektdateien hinzufügen.

---
+++ TrustSDK/TrustSDKImplementation.apple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.TrustSDK
+{
+    /// <summary>
+    /// Interface for $safeprojectgroupname$
+    /// </summary>
+    public class TrustSDKImplementation : ITrustSDK
+    {
+    }
+}
